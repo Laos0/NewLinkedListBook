2e0a41793ae8d21ff8d8a07aaba17650c70b0dfb
Student <Student>
2016-10-23 16:48:25 -0500

    Minor changes, should be 90% completed.

--- ProgramAssignmentLinkedList/BookList.cpp
+++ ProgramAssignmentLinkedList/BookList.cpp
@@ -18,7 +18,7 @@ void BookList::search(string t) {
 
 			cout << " Book found!" << endl;
 			isFound = true;
-			
+
 		}
 		else {
 			cout << " Book cannot be found! " << endl;
@@ -29,44 +29,45 @@ void BookList::search(string t) {
 	} while (currentBook->next != NULL);
 
 	//cout << first->next->title << endl;
-	
-	
-
-void BookList::insert(string t, int q, double p) {
-
-
-	if (count < 1) {
-		first = new Book;
-		first->title = t;
-		first->price = p;
-		first - next = NULL;
-		first->quantity = q;
-	}
-	else if (count == 1) {
-		last = new Book;
-		last->title = t;
-		last->next = NULL;
-		last->price = p;
-		last->quantity = q;
-		first->next = last;
-	}
-	else{
-		Book* temp = new Book;
-		temp->next = NULL;
-		temp = NULL;s
-		temp->title = t;
-		temp->price = p;
-		temp->quantity = q;
-		last->next = temp;
-		last = temp;
-	}
-
-
-
-	count++;
-
 
 }
+
+	void BookList::insert(string t, int q, double p) {
+
+
+		if (count < 1) {
+			first = new Book;
+			first->title = t;
+			first->price = p;
+			first-> next = NULL;
+			first->quantity = q;
+		}
+		else if (count == 1) {
+			last = new Book;
+			last->title = t;
+			last->next = NULL;
+			last->price = p;
+			last->quantity = q;
+			first->next = last;
+		}
+		else {
+			Book* temp = new Book;
+			temp->next = NULL;
+			temp = NULL; 
+			temp->title = t;
+			temp->price = p;
+			temp->quantity = q;
+			last->next = temp;
+			last = temp;
+		}
+
+
+
+		count++;
+
+
+	}
+
 
 void BookList::remove(string t) {
 
