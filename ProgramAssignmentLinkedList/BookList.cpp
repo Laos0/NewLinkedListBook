1af58b2333d994ef7a752747de17aa0e9a28e263
Student <Student>
2016-10-23 16:44:49 -0500

    Insert method almost completed and Search as well.

--- ProgramAssignmentLinkedList/BookList.cpp
+++ ProgramAssignmentLinkedList/BookList.cpp
@@ -11,21 +11,57 @@ BookList::~BookList() {
 }
 
 void BookList::search(string t) {
+	Book* currentBook = first;
+	bool isFound = false;
+	do {
+		if (currentBook->title == t) {
 
-}
+			cout << " Book found!" << endl;
+			isFound = true;
+			
+		}
+		else {
+			cout << " Book cannot be found! " << endl;
+			currentBook = currentBook->next;
+		}
+
+
+	} while (currentBook->next != NULL);
+
+	//cout << first->next->title << endl;
+	
+	
 
 void BookList::insert(string t, int q, double p) {
-	Book b;
-	b.title = t;
-	b.quantity = q;
-	b.price = p;
+
 
 	if (count < 1) {
-		first = &b;
-
-	}else{
-		last = &b;
+		first = new Book;
+		first->title = t;
+		first->price = p;
+		first - next = NULL;
+		first->quantity = q;
 	}
+	else if (count == 1) {
+		last = new Book;
+		last->title = t;
+		last->next = NULL;
+		last->price = p;
+		last->quantity = q;
+		first->next = last;
+	}
+	else{
+		Book* temp = new Book;
+		temp->next = NULL;
+		temp = NULL;s
+		temp->title = t;
+		temp->price = p;
+		temp->quantity = q;
+		last->next = temp;
+		last = temp;
+	}
+
+
 
 	count++;
 
--- ProgramAssignmentLinkedList/Main.cpp
+++ ProgramAssignmentLinkedList/Main.cpp
@@ -5,7 +5,22 @@ int main() {
 
 	BookList list;
 	list.insert("Rat", 3, 2.00);
+	list.insert("Dog", 5, 4.00);
+	list.insert("Cat", 3, 6.00);
+	list.insert("Crane", 3, 2.00);
+	list.insert("Snake", 5, 4.00);
+	list.insert("Frog", 3, 6.00);
+	//list.search("GOOO");
 
+	cout << list.first->next->title << endl;
+	cout << list.first->title << endl;
+	cout << list.last->title << endl;
+	cout << list.last->title << endl;
+
+	list.search("Snake");
+	
 
 	system("pause");
+
+	return 0;
 }
